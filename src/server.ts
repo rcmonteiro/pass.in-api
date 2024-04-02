44d74e022b983521048b42eee7a7a7816b9181e4
Ricardo Monteiro <rcmonteiro@gmail.com>
2024-04-02 15:15:30 -0300

    Criada as rotas para recuperar o crachá do participante e detalhes do evento

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import fastify from "fastify";
 import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
 import { createEvent } from "./routes/create-event";
+import { getAttendeeBadge } from "./routes/get-attendee-badge";
+import { getEvents } from "./routes/get-events";
 import { registerForEvent } from "./routes/register-for-event";
 
 const app = fastify()
@@ -11,6 +13,8 @@ app.setSerializerCompiler(serializerCompiler);
 
 app.register(createEvent)
 app.register(registerForEvent)
+app.register(getEvents)
+app.register(getAttendeeBadge)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server running at http://localhost:3333')
